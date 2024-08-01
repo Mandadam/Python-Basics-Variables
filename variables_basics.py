d7f013de0eb038cfdb82716db90733db62ec79b6
Mandadam <149900293+Mandadam@users.noreply.github.com>
2024-08-01 15:11:37 -0400

    Update variables_basics.py

--- variables_basics.py
+++ variables_basics.py
@@ -3,4 +3,4 @@ players = 2
 quantity = 5
 print(f"you are {age} years old")
 print(f"There are {players} players online")
-print(f"You woud like to buy {quantity} items")
+print(f"You would like to buy {quantity} items")
